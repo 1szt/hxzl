15c656eb4155850daa5c65540a4b0c26fd59e1fd
jingxuanwei <me@jingxuanwei.com>
2026-09-09 19:43:30 +0800

    feat: 添加路由支持和多个视图组件，重构项目结构

---
+++ client/src/views/TiaozhuanView.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>这是 / 根页面</h1>
+    </div>
+
+    <div class="menu">
+        <router-link to="/">标签跳转</router-link>
+        <router-link to="/" target="_blank">在新标签页中打开</router-link>
+
+
+        <button @click="goToHome">编程式跳转</button>
+
+        <button @click="goToBack">返回</button>\
+
+
+        <button @click="goToRepeat">替换 不可返回</button>
+
+
+    </div>
+</template>
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const goToHome = () => {
+    router.push('/')
+}
+
+const goToBack = () => {
+    router.go(-1)
+}
+
+const goToRepeat = () => {
+    router.replace('/')
+}
+
+</script>
